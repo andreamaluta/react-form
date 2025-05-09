1f31490a139e0312344dc60295dcf10cf2b51671
andreamaluta <andreamaluta@gmail.com>
2025-05-09 14:14:13 +0200

    [UPD] insert the array of articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 import { useState } from 'react'
 
+const articoli = [
+  {
+    "titolo": "Elezioni 2025: tutto quello che c'è da sapere",
+    "autore": "Marco Rossi",
+    "data": "2025-05-01",
+    "categoria": "Politica"
+  },
+  {
+    "titolo": "Scoperta una nuova specie di dinosauro in Patagonia",
+    "autore": "Lucia Bianchi",
+    "data": "2025-04-28",
+    "categoria": "Scienza"
+  },
+  {
+    "titolo": "Champions League: le squadre qualificate alle semifinali",
+    "autore": "Gianni Verdi",
+    "data": "2025-05-03",
+    "categoria": "Sport"
+  },
+  {
+    "titolo": "Cresce l'inflazione: cosa aspettarsi nei prossimi mesi",
+    "autore": "Francesca Neri",
+    "data": "2025-04-30",
+    "categoria": "Economia"
+  },
+  {
+    "titolo": "Festival di Cannes 2025: ecco i film più attesi",
+    "autore": "Alessandro Conti",
+    "data": "2025-05-05",
+    "categoria": "Cultura"
+  }
+]
+
 
 function App() {
 
   return (
-    <h1>Hello world</h1>
+    <h1>Articoli</h1>
+
+
+
+
   )
 
 }
