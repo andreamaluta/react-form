2a9b70c01a8bc3937ebd21e4b66e6ddea0e9168a
andreamaluta <andreamaluta@gmail.com>
2025-05-09 14:37:24 +0200

    [UPD] make the form two way use

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,35 @@ import { useState } from 'react'
 
 const articles = [
   {
+    "id": 1,
     "titolo": "Elezioni 2025: tutto quello che c'è da sapere",
     "autore": "Marco Rossi",
     "data": "2025-05-01",
     "categoria": "Politica"
   },
   {
+    "id": 2,
     "titolo": "Scoperta una nuova specie di dinosauro in Patagonia",
     "autore": "Lucia Bianchi",
     "data": "2025-04-28",
     "categoria": "Scienza"
   },
   {
+    "id": 3,
     "titolo": "Champions League: le squadre qualificate alle semifinali",
     "autore": "Gianni Verdi",
     "data": "2025-05-03",
     "categoria": "Sport"
   },
   {
+    "id": 4,
     "titolo": "Cresce l'inflazione: cosa aspettarsi nei prossimi mesi",
     "autore": "Francesca Neri",
     "data": "2025-04-30",
     "categoria": "Economia"
   },
   {
+    "id": 5,
     "titolo": "Festival di Cannes 2025: ecco i film più attesi",
     "autore": "Alessandro Conti",
     "data": "2025-05-05",
@@ -35,6 +40,8 @@ const articles = [
 
 
 function App() {
+
+  const [newArticle, setNewArticle] = useState();
 
   return (
     <div className="container">
@@ -48,7 +55,7 @@ function App() {
       </ul>
       <form>
         <div className="input-group mt-3">
-          <input type="text" className='form-control' placeholder='Nuovo Articolo' />
+          <input onChange={(e) => { setNewArticle(e.target.value) }} type="text" className='form-control' placeholder='Nuovo Articolo' />
           <button className='btn btn-primary'>Add Article</button>
         </div>
       </form>
